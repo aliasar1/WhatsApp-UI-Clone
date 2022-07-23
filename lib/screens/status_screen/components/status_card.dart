794d95313e5a1ef50c881a3c719c6653ab34f712
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2022-07-23 15:01:43 +0500

    Fixed Files.

---
+++ lib/screens/status_screen/components/status_card.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../models/Status.dart';
+
+class PublicStatusCard extends StatelessWidget {
+  const PublicStatusCard({
+    Key? key,
+    required this.status,
+  }) : super(key: key);
+
+  final Status status;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: kSmallPadding, vertical: kSmallPadding * 0.7),
+      child: Row(
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            decoration: status.isSeen == true
+                ? BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: kSecondaryColor,
+                      width: 3,
+                    ))
+                : BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: kSeenColor,
+                      width: 3,
+                    )),
+            child: CircleAvatar(
+              radius: 26,
+              backgroundImage: AssetImage(status.image),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: kSmallPadding, vertical: kSmallPadding * 0.7),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  status.name,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w500, fontSize: 16),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Opacity(
+                  opacity: 0.6,
+                  child: Text(
+                    status.time,
+                    maxLines: 1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/status_screen/status_screen.dart
+++ lib/screens/status_screen/status_screen.dart
@@ -3,6 +3,8 @@ import 'package:whatsapp_clone/constants.dart';
 
 import '../../models/Status.dart';
 import 'components/profile_card.dart';
+import 'components/status_builder.dart';
+import 'components/status_card.dart';
 
 class StatusScreen extends StatefulWidget {
   const StatusScreen({Key? key}) : super(key: key);
@@ -71,104 +73,6 @@ class _StatusScreenState extends State<StatusScreen> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class PublicStatusCard extends StatelessWidget {
-  const PublicStatusCard({
-    Key? key,
-    required this.status,
-  }) : super(key: key);
-
-  final Status status;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-          horizontal: kSmallPadding, vertical: kSmallPadding * 0.7),
-      child: Row(
-        children: [
-          Container(
-            height: 50,
-            width: 50,
-            decoration: status.isSeen == true
-                ? BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: kSecondaryColor,
-                      width: 3,
-                    ))
-                : BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: kSeenColor,
-                      width: 3,
-                    )),
-            child: CircleAvatar(
-              radius: 26,
-              backgroundImage: AssetImage(status.image),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: kSmallPadding, vertical: kSmallPadding * 0.7),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  status.name,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w500, fontSize: 16),
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                Opacity(
-                  opacity: 0.6,
-                  child: Text(
-                    status.time,
-                    maxLines: 1,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class StatusBuilder extends StatelessWidget {
-  const StatusBuilder({
-    Key? key,
-    required this.text,
-  }) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.centerLeft,
-          width: double.infinity,
-          height: 35,
-          color: kShadeColor,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: kSmallPadding * 1.5,
-            ),
-            child: Text(
-              text,
-              style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 15),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
