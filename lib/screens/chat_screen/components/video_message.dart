81835abe4d54c40a6983e648b06f0f2e95616814
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2022-07-23 16:43:40 +0500

    Working with video messages.

--- lib/screens/chat_screen/components/video_message.dart
+++ lib/screens/chat_screen/components/video_message.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 import '../../../models/ChatMessage.dart';
 
-class VideoMessage extends StatelessWidget {
+class VideoMessage extends StatefulWidget {
   const VideoMessage({Key? key, required this.message}) : super(key: key);
   final ChatMessage message;
 
+  @override
+  State<VideoMessage> createState() => _VideoMessageState();
+}
+
+class _VideoMessageState extends State<VideoMessage> {
   @override
   Widget build(BuildContext context) {
     return Container();
