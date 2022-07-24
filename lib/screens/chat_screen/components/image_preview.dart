112558d4cc1e646c8c39f349704278c33157efab
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2022-07-24 22:25:06 +0500

    Added gesture detector on image viewer.

--- lib/screens/chat_screen/components/image_preview.dart
+++ lib/screens/chat_screen/components/image_preview.dart
@@ -22,60 +22,79 @@ class _ImageViewerState extends State<ImageViewer> {
   @override
   Widget build(BuildContext context) {
     bool isStarred = false;
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: kAppBarColor,
-        elevation: 0,
-        title: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            widget.isSender
-                ? const Align(
-                    alignment: Alignment.centerLeft, child: Text("You"))
-                : Align(
-                    alignment: Alignment.centerLeft, child: Text(widget.name)),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Opacity(
-                  opacity: 0.6,
-                  child: Text(
-                    widget.time,
-                    style: const TextStyle(fontSize: 14),
-                  )),
-            )
+    return GestureDetector(
+      onHorizontalDragUpdate: (details) {
+        int sensitivity = 8;
+        if (details.delta.dx > sensitivity) {
+          Navigator.pop(context);
+        } else if (details.delta.dx < -sensitivity) {
+          Navigator.pop(context);
+        }
+      },
+      onVerticalDragUpdate: (details) {
+        int sensitivity = 8;
+        if (details.delta.dy > sensitivity) {
+          Navigator.pop(context);
+        } else if (details.delta.dy < -sensitivity) {
+          Navigator.pop(context);
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: kAppBarColor,
+          elevation: 0,
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              widget.isSender
+                  ? const Align(
+                      alignment: Alignment.centerLeft, child: Text("You"))
+                  : Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(widget.name)),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Opacity(
+                    opacity: 0.6,
+                    child: Text(
+                      widget.time,
+                      style: const TextStyle(fontSize: 14),
+                    )),
+              )
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  isStarred = !isStarred;
+                });
+              },
+              icon: isStarred == false
+                  ? const Icon(Icons.grade_outlined, color: kBackgroundColor)
+                  : const Icon(Icons.grade, color: kBackgroundColor),
+            ),
+            IconButton(
+              icon: const Icon(Icons.forward, color: kBackgroundColor),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.more_vert, color: kBackgroundColor),
+              onPressed: () {},
+            ),
           ],
         ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              setState(() {
-                isStarred = !isStarred;
-              });
-            },
-            icon: isStarred == true
-                ? const Icon(Icons.grade_outlined, color: kBackgroundColor)
-                : const Icon(Icons.grade, color: kBackgroundColor),
-          ),
-          IconButton(
-            icon: const Icon(Icons.forward, color: kBackgroundColor),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.more_vert, color: kBackgroundColor),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Align(
-        alignment: Alignment.center,
-        child: AspectRatio(
-          aspectRatio: 4 / 3,
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(widget.image),
-                fit: BoxFit.cover,
+        body: Align(
+          alignment: Alignment.center,
+          child: AspectRatio(
+            aspectRatio: 4 / 3,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(widget.image),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
