86d0ada825ea9955a698ccedc6f09152e1826fd0
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2022-08-17 17:06:51 +0500

    Added caller screen.

--- lib/constants.dart
+++ lib/constants.dart
@@ -17,6 +17,7 @@ const kReceiverMessageColor = Color.fromARGB(255, 205, 241, 198);
 const kHighlightColor = Color.fromARGB(255, 40, 153, 65);
 const kDarkGreyColor = Color.fromARGB(255, 106, 110, 112);
 const kYellowColor = Color.fromARGB(255, 235, 221, 183);
+const kDraggableSheetColor = Color.fromARGB(255, 104, 111, 122);
 
 const kSmallPadding = 8.0;
 const kMedPadding = 12.0;
--- lib/screens/caller_screen/caller_screen.dart
+++ lib/screens/caller_screen/caller_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/constants.dart';
 
-class CallerScreen extends StatelessWidget {
+class CallerScreen extends StatefulWidget {
   const CallerScreen({Key? key}) : super(key: key);
 
   @override
+  State<CallerScreen> createState() => _CallerScreenState();
+}
+
+class _CallerScreenState extends State<CallerScreen> {
+  @override
   Widget build(BuildContext context) {
+    bool micOff = true;
+
     return SafeArea(
       child: Container(
         decoration: const BoxDecoration(
@@ -12,6 +20,168 @@ class CallerScreen extends StatelessWidget {
               opacity: 0.45,
               image: AssetImage('assets/images/dark background.png'),
               fit: BoxFit.cover),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          resizeToAvoidBottomInset: true,
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: kLargePadding * 4,
+                ),
+                child: Container(
+                  alignment: Alignment.topCenter,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  height: MediaQuery.of(context).size.height * 0.24,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      CircleAvatar(
+                        radius: 40,
+                        backgroundImage: AssetImage("assets/images/bill.jpg"),
+                      ),
+                      Text(
+                        "Ali Asar",
+                        style: TextStyle(
+                            color: kBackgroundColor,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Opacity(
+                        opacity: 0.7,
+                        child: Text(
+                          "Ringing",
+                          style: TextStyle(
+                            color: kBackgroundColor,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: DraggableScrollableSheet(
+                  initialChildSize: 0.22,
+                  minChildSize: 0.22,
+                  maxChildSize: 0.6,
+                  builder: (BuildContext context, myscrollController) {
+                    return Container(
+                      decoration: const BoxDecoration(
+                        color: kDraggableSheetColor,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(15),
+                            topLeft: Radius.circular(15)),
+                      ),
+                      child: ListView.builder(
+                        controller: myscrollController,
+                        itemCount: 1,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Column(
+                            children: [
+                              const Icon(
+                                Icons.expand_less,
+                                size: 36,
+                                color: kBackgroundColor,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    bottom: kLargePadding),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    const Icon(
+                                      Icons.volume_up,
+                                      size: 28,
+                                      color: kBackgroundColor,
+                                    ),
+                                    const Icon(
+                                      Icons.videocam_rounded,
+                                      size: 28,
+                                      color: kBackgroundColor,
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        micOff ? Icons.mic_off : Icons.mic,
+                                      ),
+                                      onPressed: () {
+                                        setState(() {
+                                          micOff = !micOff;
+                                        });
+                                      },
+                                    ),
+                                    Container(
+                                      height: 40,
+                                      width: 40,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: kRedColor.withOpacity(0.7),
+                                      ),
+                                      child: const Icon(
+                                        Icons.call_end_rounded,
+                                        size: 28,
+                                        color: kBackgroundColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const Divider(
+                                color: kIconColor,
+                                height: 1,
+                              ),
+                              ListTile(
+                                leading: Container(
+                                  height: 38,
+                                  width: 38,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: kPrimaryColor,
+                                  ),
+                                  child: const Icon(
+                                    Icons.person_add,
+                                    size: 22,
+                                    color: kBackgroundColor,
+                                  ),
+                                ),
+                                title: const Text(
+                                  "Add participants",
+                                  style: TextStyle(
+                                      color: kBackgroundColor,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                              const ListTile(
+                                leading: CircleAvatar(
+                                  radius: 18,
+                                  backgroundImage:
+                                      AssetImage("assets/images/bill.jpg"),
+                                ),
+                                title: Text(
+                                  "Ali Asar",
+                                  style: TextStyle(
+                                      color: kSeenColor, fontSize: 16),
+                                ),
+                                trailing: Icon(
+                                  Icons.more_horiz,
+                                  size: 26,
+                                  color: kSeenColor,
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
